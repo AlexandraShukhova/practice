b1331076e79d121d96ebd52aaa09370dec96fe0b
AlexandraShukhova <44906072+AlexandraShukhova@users.noreply.github.com>
2020-06-30 22:19:20 +0300

    Add files via upload

---
+++ day1/camMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class camMove : MonoBehaviour
+{
+    public GameObject player;
+
+    void Update()
+    {
+        //движение камеры за объектом
+        transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, -10f);  
+    }
+}
